8efd2d528f00a024267fd0a186743b06f95cb7b8
IvanGorbunov <ivan_0110@mail.ru>
2022-04-22 10:33:41 +0300

    added app blog

--- DRF_blog/urls.py
+++ DRF_blog/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('DRF_blog.urls_api')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
